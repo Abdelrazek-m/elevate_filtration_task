79668201777abf5d7425b002b3cb16e5dfc3e14d
Abdelrazek Mahmoud <Aalrazek02@gmail.com>
2026-02-02 13:59:50 +0200

    setup project & dependencies & add ( apiService & productsModel & ProductsRepo)

---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,34 @@
+import 'dart:developer';
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final dio = Dio();
+  final String baseUrl = 'https://fakestoreapi.com/products';
+
+  Future<dynamic> get({
+    required String endPoint,
+  }) async {
+    var headers = {
+      'Content-Type': 'application/json',
+    };
+    try {
+      var response = await dio.request('$baseUrl$endPoint',
+          options: Options(
+            method: 'Get',
+            headers: headers,
+          ),
+          );
+      if (response.statusCode == 400) {
+        return 'Error 400';
+      }
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        log("${response.statusMessage}");
+      }
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+}
---
+++ lib/feature/products/data/repo/products_repo_impl.dart
@@ -0,0 +1,10 @@
+import 'package:elevate_filtration_task/feature/products/data/models/product_model.dart';
+
+import 'products_repo.dart';
+
+class ProductsRepoImpl extends ProductsRepo {
+  @override
+  Future<List<ProductModel>> getProductsList() {
+    
+  }
+}
